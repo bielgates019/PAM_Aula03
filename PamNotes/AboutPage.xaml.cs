8fe826188f167eb44d59017fcd13974d2c7c9ebb
RoNeves69 <RonaldNeves69@outlook.com>
2024-08-16 22:33:29 -0300

    Aula04

---
+++ PamNotes/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace PamNotes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void AboutButton_Clicked(object sender, EventArgs e)
+	{
+		await Launcher.Default.OpenAsync("https://github.com/RoNeves69");
+	}
+
+}
